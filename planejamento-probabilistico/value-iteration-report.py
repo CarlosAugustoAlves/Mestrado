913a9481cbd7b63cc0022ad18255a5568022855a
CarlosAugustoAlves <carlos.alves91@gmail.com>
2019-04-19 15:45:46 -0300

    Impressão do resultado

--- planejamento-probabilistico/value-iteration-report.py
+++ planejamento-probabilistico/value-iteration-report.py
@@ -1,6 +1,7 @@
 import collections
 import numpy
 from pathlib import Path
+import matplotlib.pylab as plt
 
 current_path = Path().absolute()
 
@@ -12,31 +13,54 @@ epsilon = 0.00001
 states = [state for state in range(135)]
 actions = [0, 1, 2, 3, 4, 5]  # North, South, East, West, Up, Down
 directory_report = str(current_path) + "\Dados_Relatorio1\Ambiente1"
+matrix_draw_max_x = 15
+matrix_draw_max_y = 9
 
-# Example for Ambiente0
-# directory_report = str(current_path) + "\Dados_Relatorio1\Ambiente1" -> folder data
-# states = [state for state in range(10)] -> count states
+# Run Ambiente0
+# directory_report = str(current_path) + "\Dados_Relatorio1\Ambiente0"  # -> folder data
+# states = [state for state in range(10)]  # -> count states
 # actions = [0, 1, 2, 3]  # North, South, East, West -> vector actions
+# matrix_draw_max_x = 5
+# matrix_draw_max_y = 2
 
 states_count = len(states)
 actions_count = len(actions)
-
 transition_matrix = numpy.zeros((states_count, actions_count, states_count))
-
-for i in range(actions_count):
-    file_action_transition = open(directory_report + "\Action0" +
-                                  str(i + 1) + ".txt", "r")
-    file_info = file_action_transition.readlines()
-    for line in file_info:
-        state_from = int(float(line.split()[0])) - 1
-        state_to = int(float(line.split()[1])) - 1
-        state_probability = float(line.split()[2])
-        transition_matrix[state_from, i, state_to] = state_probability
-
 reward = []
-file_reward = open(directory_report + "\Rewards.txt", "r")
-for line in file_reward.readlines():
-    reward.append(float(line))
+
+
+def fill_transition_matrix():
+    for i in range(actions_count):
+        file_action_transition = open(directory_report + "\Action0" +
+                                      str(i + 1) + ".txt", "r")
+        file_info = file_action_transition.readlines()
+        for line in file_info:
+            state_from = int(float(line.split()[0])) - 1
+            state_to = int(float(line.split()[1])) - 1
+            state_probability = float(line.split()[2])
+            transition_matrix[state_from, i, state_to] = state_probability
+
+
+def fill_reward_states():
+    file_reward = open(directory_report + "\Rewards.txt", "r")
+    for line in file_reward.readlines():
+        reward.append(float(line))
+
+def draw_result_figure():
+    matrix_draw_result = numpy.array(
+        Value[len(Value) - 1]).reshape((matrix_draw_max_y, matrix_draw_max_x))
+
+    img = plt.imshow(matrix_draw_result, interpolation='nearest')
+    img.set_cmap('hot')
+    img.axes.get_xaxis().set_visible(False)
+    img.axes.get_yaxis().set_visible(False)
+    # remove rectangle borders and axes plt.axis('off')
+    plt.savefig(directory_report + "\ResultImage.png")
+    #plt.show()
+
+fill_transition_matrix()
+
+fill_reward_states()
 
 # First iteration: initialize values arbitrarily, e.g., zero
 Value = [[0.0] * states_count]
@@ -44,12 +68,9 @@ policy = [None for state in range(states_count)]
 
 iteration = 0
 continue_iteration = True
-
 file_results = open(directory_report + "\Results.txt", "a")
-for line in file_reward.readlines():
-    reward.append(float(line))
 
-while continue_iteration == True:
+while continue_iteration:
     iteration += 1
     continue_iteration = False
 
@@ -84,3 +105,5 @@ while continue_iteration == True:
 
 
 file_results.close()
+
+draw_result_figure()
